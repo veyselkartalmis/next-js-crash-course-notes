e5fc7d510763ba0ecebf8d47e3ddd6e03a43cd67
Veysel <veyselkartalms@gmail.com>
2022-08-13 19:04:30 +0300

    Router

---
+++ pages/blog/index.jsx
@@ -0,0 +1,5 @@
+export default function Index() {
+    return <div>
+        <h1>BLOG PAGE</h1>
+    </div>
+}
---
+++ pages/blog/post.jsx
@@ -0,0 +1,5 @@
+export default function Post() {
+    return <div>
+        <h1>POST PAGE</h1>
+    </div>
+}
