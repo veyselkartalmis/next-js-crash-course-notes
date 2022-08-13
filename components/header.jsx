05d7583b74eec60b9a4e07225f6c457f6375b53c
Veysel <veyselkartalms@gmail.com>
2022-08-13 21:27:27 +0300

    some style code

--- components/header.jsx
+++ components/header.jsx
@@ -2,15 +2,31 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header>
+        <header className="header">
             <nav>
                 <ul>
-                    <li><Link href="/">Homepage</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                    <li><Link href="/blog">Blog</Link></li>
-                    <li><Link href="/blog/post">Post</Link></li>
+                    <li><Link href="/"><a>Homepage</a></Link></li>
+                    <li><Link href="/about"><a>About</a></Link></li>
+                    <li><Link href="/blog"><a>Blog</a></Link></li>
+                    <li><Link href="/blog/post"><a>Post</a></Link></li>
                 </ul>
             </nav>
+            <style jsx>{`
+                .header {
+                    border-bottom: 1px solid #ddd;
+                    background: #fff 
+                }
+
+                .header ul {display: flex;}
+                .header ul li a {
+                    display: flex;
+                    text-decoration: none;
+                    height: 60px;
+                    align-items: center;
+                    padding: 0 20px;
+                    color: #333;
+                }
+            `}</style>
         </header>
     )
 }
